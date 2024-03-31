bb5087cb6a4fb25eb1248257682cff9f5cb448a0
jongmee <ra123221@gmail.com>
2024-03-31 16:12:20 +0900

    test(ChessGameServiceTest): ChessGameService 통합 테스트 추가

---
+++ src/test/java/chess/service/ChessGameServiceTest.java
@@ -0,0 +1,154 @@
+package chess.service;
+
+import chess.model.board.*;
+import chess.model.evaluation.GameResult;
+import chess.model.piece.Blank;
+import chess.model.piece.Pawn;
+import chess.model.piece.Piece;
+import chess.model.piece.Side;
+import chess.model.position.File;
+import chess.model.position.Position;
+import chess.model.position.Rank;
+import chess.repository.DataBaseCleaner;
+import chess.repository.dao.ChessBoardDao;
+import chess.repository.dao.PieceDao;
+import chess.repository.dao.TurnDao;
+import chess.repository.dto.GameResultDto;
+import chess.repository.utility.MySqlConnector;
+import chess.view.input.MoveArguments;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ChessGameServiceTest {
+    private final DataBaseCleaner dataBaseCleaner = new DataBaseCleaner();
+    private final ChessGameService chessGameService = new ChessGameService(MySqlConnector.TEST_CONNECTION);
+    private final ChessBoardDao chessBoardDao = new ChessBoardDao(MySqlConnector.TEST_CONNECTION);
+    private final PieceDao pieceDao = new PieceDao(MySqlConnector.TEST_CONNECTION);
+    private final TurnDao turnDao = new TurnDao(MySqlConnector.TEST_CONNECTION);
+
+    @BeforeEach
+    void setUp() {
+        dataBaseCleaner.truncateTables();
+    }
+
+    @Test
+    @DisplayName("이전에 게임하던 체스판이 존재하지 않으면 체스판을 생성한다.")
+    void createInitialChessBoard() {
+        // when
+        ChessBoard chessBoard = chessGameService.createOrGetInitialChessBoard();
+
+        // then
+        assertThat(chessBoard.getBoard()).hasSize(64);
+    }
+
+    @Test
+    @DisplayName("이전에 게임하던 체스판이 존재하면 체스판을 조회한다.")
+    void getInitialChessBoard() {
+        // given
+        ChessBoard existingChessBoard = createTestChessBoard();
+        long expectedChessBoardId = existingChessBoard.getId();
+
+        // when
+        ChessBoard chessBoard = chessGameService.createOrGetInitialChessBoard();
+
+        // then
+        long actualChessBoardId = chessBoard.getId();
+        assertThat(actualChessBoardId).isSameAs(expectedChessBoardId);
+    }
+
+    @Test
+    @DisplayName("기물을 사용자의 입력으로 이동시킨다.")
+    void move() {
+        // given
+        ChessBoard existingChessBoard = createTestChessBoard();
+        Turn turn = Turn.from(Side.WHITE);
+        MoveArguments moveArguments = new MoveArguments("b", 2, "b", 3);
+
+        // when
+        chessGameService.move(existingChessBoard, turn, moveArguments);
+
+        // then
+        ChessBoard chessBoard = chessBoardDao.findLatest().get();
+        assertThat(chessBoard.getBoard())
+                .containsAllEntriesOf(Map.of(
+                        Position.of(File.B, Rank.THREE), Pawn.from(Side.WHITE),
+                        Position.of(File.B, Rank.TWO), Blank.INSTANCE)
+                );
+    }
+
+    @Test
+    @DisplayName("체스판의 차례가 생성되지 않았다면 WHITE 차례를 생성한다.")
+    void createInitialTurn() {
+        // given
+        ChessBoard chessBoard = createTestChessBoard();
+
+        // when
+        Turn turn = chessGameService.createOrGetInitialTurn(chessBoard);
+
+        // then
+        assertThat(turn).isEqualTo(Turn.from(Side.WHITE));
+    }
+
+    @Test
+    @DisplayName("체스판의 순서가 생성되었다면 조회한다.")
+    void getInitialTurn() {
+        // given
+        ChessBoard chessBoard = createTestChessBoard();
+        Turn initialTurn = Turn.from(Side.WHITE);
+        turnDao.save(chessBoard.getId(), initialTurn);
+
+        // when
+        Turn turn = chessGameService.createOrGetInitialTurn(chessBoard);
+
+        // then
+        assertThat(turn).isEqualTo(Turn.from(Side.WHITE));
+    }
+
+    @Test
+    @DisplayName("체스판의 다음 순서를 저장한다.")
+    void saveNextTurn() {
+        // given
+        ChessBoard chessBoard = createTestChessBoard();
+        Turn turn = Turn.from(Side.WHITE);
+
+        // when
+        Turn nextTurn = chessGameService.saveNextTurn(chessBoard, turn);
+
+        // then
+        assertThat(nextTurn).isEqualTo(Turn.from(Side.BLACK));
+    }
+
+    @Test
+    @DisplayName("체스 게임 결과를 저장한다.")
+    void saveGameResult() {
+        // given
+        Map<Position, Piece> allPieces = new TestChessBoardGenerator(ChessBoardFixture.WHITE_FOOLS_MATE_LOSE).create();
+        long chessBoardId = chessBoardDao.save().get();
+        pieceDao.saveAll(allPieces, chessBoardId);
+
+        ChessBoard chessBoard = new ChessBoard(chessBoardId, allPieces);
+
+        // when
+        chessGameService.saveGameResult(chessBoard);
+
+        // then
+        List<GameResultDto> gameResults = chessBoardDao.findAllGameResult();
+        assertThat(gameResults).hasSize(1)
+                .extracting(GameResultDto::gameResult)
+                .contains(GameResult.BLACK_WIN);
+    }
+
+    private ChessBoard createTestChessBoard() {
+        ChessBoardInitializer chessBoardInitializer = new ChessBoardInitializer();
+        Map<Position, Piece> allPieces = chessBoardInitializer.create();
+        long chessBoardId = chessBoardDao.save().get();
+        pieceDao.saveAll(allPieces, chessBoardId);
+        return new ChessBoard(chessBoardId, allPieces);
+    }
+}
