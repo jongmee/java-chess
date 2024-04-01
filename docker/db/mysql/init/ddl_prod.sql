1fb678919138f2103d8b4b667836f39ef196636d
jongmee <ra123221@gmail.com>
2024-04-01 14:35:58 +0900

    chore: test용 ddl 별개의 파일로 분리

--- docker/db/mysql/init/ddl_prod.sql
+++ docker/db/mysql/init/ddl_prod.sql
@@ -2,38 +2,8 @@ GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';
 FLUSH PRIVILEGES;
 
 CREATE DATABASE IF NOT EXISTS `chess` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE DATABASE IF NOT EXISTS `chess_test` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 USE chess;
-
-CREATE TABLE chess_board
-(
-    chess_board_id BIGINT      NOT NULL AUTO_INCREMENT,
-    created_at     TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    game_result    VARCHAR(15) NOT NULL,
-    PRIMARY KEY (chess_board_id)
-);
-
-CREATE TABLE piece
-(
-    file           VARCHAR(10) NOT NULL,
-    `rank`         INT         NOT NULL,
-    type           VARCHAR(10) NOT NULL,
-    chess_board_id BIGINT      NOT NULL,
-    side           VARCHAR(10) NOT NULL,
-    FOREIGN KEY (`chess_board_id`) REFERENCES `chess_board` (`chess_board_id`),
-    PRIMARY KEY (file, `rank`, chess_board_id)
-);
-
-CREATE TABLE turn
-(
-    side           VARCHAR(10) NOT NULL,
-    chess_board_id BIGINT      NOT NULL,
-    FOREIGN KEY (chess_board_id) REFERENCES `chess_board` (`chess_board_id`),
-    PRIMARY KEY (chess_board_id)
-);
-
-USE chess_test;
 
 CREATE TABLE chess_board
 (
